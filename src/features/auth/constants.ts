533efdeea034f73685a4454aa16bf1c93f794691
Rakesh Tripathi <tripathirakeshofficial@gmail.com>
2024-12-09 15:07:46 +0530

    Setting up Appwrite & database

---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "tripathirakeshofficial-jira-clone-session";
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,24 +1,58 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
+import { ID } from "node-appwrite";
+import { deleteCookie, setCookie } from "hono/cookie";
+
+import { createAdminClient } from "@/lib/appwrite";
 
 import { loginSchema, registerSchema } from "../schemas";
+import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = await c.req.valid("json");
 
+    const { account } = await createAdminClient();
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
     return c.json({
-      email,
-      password,
+      success: true,
     });
   })
   .post("/register", zValidator("json", registerSchema), async (c) => {
     const { name, email, password } = await c.req.valid("json");
 
+    const { account } = await createAdminClient();
+
+    await account.create(ID.unique(), email, password, name);
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
     return c.json({
-      name,
-      email,
-      password,
+      success: true,
+    });
+  })
+  .post("/logout", (c) => {
+    deleteCookie(c, AUTH_COOKIE);
+
+    return c.json({
+      success: true,
     });
   });
 

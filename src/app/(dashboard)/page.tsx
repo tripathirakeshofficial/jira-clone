ec3ece65fbe136220272f3c4682965dd8e6ed51c
Rakesh Tripathi <tripathirakeshofficial@gmail.com>
2024-12-19 14:40:41 +0530

    Building a dashboard layout

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,16 +1,11 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/actions";
-import { UserButton } from "@/features/auth/components/user-button";
 
 export default async function Home() {
   const user = await getCurrent();
 
   if (!user) return redirect("/sign-in");
 
-  return (
-    <div>
-      <UserButton />
-    </div>
-  );
+  return <div>This is a home page</div>;
 }
